83d6aed02662b6189c2d490582df3f55c8789ab9
avishn <avishn@ba2e8461-9649-0410-aa75-97a1ba0a621a>
2008-04-15 21:50:38 +0000

    antlr

---
+++ modsl-core/src/test/java/org/modsl/antlr/AntlrTestCase.java
@@ -0,0 +1,17 @@
+package org.modsl.antlr;
+
+import junit.framework.TestCase;
+
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.CommonTokenStream;
+
+public class AntlrTestCase extends TestCase {
+
+	public void testExpr() {
+		/*ANTLRInputStream input = new ANTLRInputStream(System.in);
+		ExprLexer lexer = new ExprLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		ExprParser parser = new ExprParser(tokens);
+		parser.prog();*/
+	}
+}
