46f0eea9baf920d7ad0b022791cbc596dad3df80
avishn <avishn@ba2e8461-9649-0410-aa75-97a1ba0a621a>
2008-04-19 00:26:26 +0000

    config refactoring 16

--- modsl-core/src/main/java/org/modsl/core/svg/AbstractSvgWriter.java
+++ modsl-core/src/main/java/org/modsl/core/svg/AbstractSvgWriter.java
@@ -35,66 +35,66 @@ import org.modsl.core.model.diagram.Diagram;
 
 public abstract class AbstractSvgWriter<D extends Diagram, P extends AbstractProps> {
 
-    protected static final String TEMPLATE_FILE_EXT = ".tpl";
-    protected static GStringTemplateEngine engine = new GStringTemplateEngine();
+	protected static final String TEMPLATE_FILE_EXT = ".tpl";
+	protected static GStringTemplateEngine engine = new GStringTemplateEngine();
 
-    private final Logger log = Logger.getLogger(getClass());
+	private final Logger log = Logger.getLogger(getClass());
 
-    protected Map<String, groovy.text.Template> templateCache = new TreeMap<String, groovy.text.Template>();
-    protected P props;
+	protected Map<String, groovy.text.Template> templateCache = new TreeMap<String, groovy.text.Template>();
+	protected P props;
 
-    public AbstractSvgWriter(P props) {
-        this.props = props;
-    }
+	public AbstractSvgWriter(P props) {
+		this.props = props;
+	}
 
-    public abstract String render(D diagram);
+	public abstract String render(D diagram);
 
-    public String renderToFile(D diagram, String fileName) throws FileNotFoundException {
-        PrintStream p = new PrintStream(new FileOutputStream(fileName));
-        String str = render(diagram);
-        p.print(str);
-        p.close();
-        return str;
-    }
+	public String renderToFile(D diagram, String fileName) throws FileNotFoundException {
+		PrintStream p = new PrintStream(new FileOutputStream(fileName));
+		String str = render(diagram);
+		p.print(str);
+		p.close();
+		return str;
+	}
 
-    protected void invokeTemplate(StringBuffer sb, AbstractDiagramObject<?> diagramObject, String diagramObjectName,
-            String templateName) {
-        if (diagramObject.isVisible()) {
-            Map<String, Object> bindings = bind(diagramObjectName, diagramObject);
-            String fullTemplateName = getTemplateFileName(templateName);
-            try {
-                groovy.text.Template template = getTemplate(fullTemplateName);
-                sb.append(template.make(bindings));
-            } catch (Exception ex) {
-                log.debug("Invoking template " + fullTemplateName + " with binding " + diagramObjectName + "("
-                        + diagramObject.getClass() + ")", ex);
-            }
-        }
-    }
+	protected void invokeTemplate(StringBuffer sb, AbstractDiagramObject<?> diagramObject, String diagramObjectName,
+			String templateName) {
+		if (diagramObject.isVisible()) {
+			Map<String, Object> bindings = bind(diagramObjectName, diagramObject);
+			String fullTemplateName = getTemplateFileName(templateName);
+			try {
+				groovy.text.Template template = getTemplate(fullTemplateName);
+				sb.append(template.make(bindings));
+			} catch (Exception ex) {
+				log.debug("Invoking template " + fullTemplateName + " with binding " + diagramObjectName + "("
+						+ diagramObject.getClass() + ")", ex);
+			}
+		}
+	}
 
-    private synchronized groovy.text.Template getTemplate(String fullTemplateName) throws Exception {
-        groovy.text.Template template = templateCache.get(fullTemplateName);
-        if (template == null) {
-            InputStream is = getClass().getResourceAsStream(fullTemplateName);
-            if (is == null) {
-                throw new RuntimeException("Cannot find template " + fullTemplateName);
-            }
-            BufferedReader r = new BufferedReader(new InputStreamReader(is));
-            template = engine.createTemplate(r);
-            templateCache.put(fullTemplateName, template);
-            r.close();
-        }
-        return template;
-    }
+	private synchronized groovy.text.Template getTemplate(String fullTemplateName) throws Exception {
+		groovy.text.Template template = templateCache.get(fullTemplateName);
+		if (template == null) {
+			InputStream is = getClass().getResourceAsStream(fullTemplateName);
+			if (is == null) {
+				throw new RuntimeException("Cannot find template " + fullTemplateName);
+			}
+			BufferedReader r = new BufferedReader(new InputStreamReader(is));
+			template = engine.createTemplate(r);
+			templateCache.put(fullTemplateName, template);
+			r.close();
+		}
+		return template;
+	}
 
-    private String getTemplateFileName(String element) {
-        return props.getPath() + element + TEMPLATE_FILE_EXT;
-    }
+	private String getTemplateFileName(String element) {
+		return props.getPath() + "/" + props.getName() + "/" + element + TEMPLATE_FILE_EXT;
+	}
 
-    private Map<String, Object> bind(String key, Object value) {
-        Map<String, Object> b = new HashMap<String, Object>();
-        b.put("config", props);
-        b.put(key, value);
-        return b;
-    }
+	private Map<String, Object> bind(String key, Object value) {
+		Map<String, Object> b = new HashMap<String, Object>();
+		b.put("config", props);
+		b.put(key, value);
+		return b;
+	}
 }
