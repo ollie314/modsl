cb26e36adb18f4f32a4165239f7b9c8a31b518ad
avishn <avishn@ba2e8461-9649-0410-aa75-97a1ba0a621a>
2008-04-19 13:36:37 +0000

    config junits

---
+++ trunk/modsl-core/src/test/java/org/modsl/core/config/LayoutPropsTest.java
@@ -0,0 +1,25 @@
+package org.modsl.core.config;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LayoutPropsTest {
+
+	@Test
+	public void layout() {
+		LP lp = new LP("/config", "test");
+		assertEquals(lp.getProp("layout1"), "default1");
+		assertEquals(lp.getProp("layout2"), "test2");
+		assertEquals(lp.getProp("layout3"), "test3");
+	}
+
+	private static class LP extends AbstractLayoutProps {
+
+		public LP(String path, String name) {
+			super(path, name);
+		}
+
+	}
+
+}
