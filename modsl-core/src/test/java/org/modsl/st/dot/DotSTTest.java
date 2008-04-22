83d59ce6b6e5305c4be46acffb70749a4baf8b81
avishn <avishn@ba2e8461-9649-0410-aa75-97a1ba0a621a>
2008-04-22 17:30:23 +0000

    added stringtemplate 3.1 dependency

---
+++ modsl-core/src/test/java/org/modsl/st/dot/DotSTTest.java
@@ -0,0 +1,9 @@
+package org.modsl.st.dot;
+
+import org.antlr.stringtemplate.StringTemplateGroup;
+
+public class DotSTTest {
+
+    protected StringTemplateGroup group = new StringTemplateGroup("dot", "/tmp");
+
+}
